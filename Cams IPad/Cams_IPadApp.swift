49f7cb282fa6a78523077eb7f1e9386607d46046
Ben <benlevinmarketing@gmail.com>
2025-03-24 09:53:19 -0400

    Initial Commit

---
+++ Cams IPad/Cams_IPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cams_IPadApp.swift
+//  Cams IPad
+//
+//  Created by Ben Levin on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Cams_IPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cams IPad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cams IPad
+//
+//  Created by Ben Levin on 3/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
